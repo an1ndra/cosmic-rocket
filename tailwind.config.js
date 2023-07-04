efe6a28d3cd278e49db66db3c9f4109c2a467fe5
Anindra <anindrakarmakar+git@proton.me>
2023-07-04 13:27:14 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./build/*.html'],
+  // content: ['./build/*.html','./build/js/*.js'],
+  // alternative
+  content:["./build/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ration:3/2)' },
+        'tallscreen': { 'raw': '(max-aspect-ration:13/20)' }
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform: 'scaleY(0)' },
+          '8%': { transform: 'scaleY(1.2)' },
+          '100%': { transform: 'scaleY(1)' },
+        },
+      },
+      animation:{
+        'open-menu':'open-menu 0.5s ease-in-out forwards',
+      }
+    },
   },
   plugins: [],
 }
